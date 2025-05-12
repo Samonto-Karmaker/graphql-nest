2007cb39922ba3a0fdf83bb77a0f96beb00e11a2
Samonto Karmaker <samonto0726@gmail.com>
2025-05-12 12:00:36 +0600

    feat: add mock user preference data for testing

---
+++ src/data/mockUserPreference.ts
@@ -0,0 +1,23 @@
+export const mockUserPreference = [
+    {
+        id: 1,
+        userId: 1,
+        isDarKTheme: true,
+        isEmailEnable: true,
+        isPushEnable: true,
+    },
+    {
+        id: 2,
+        userId: 3,
+        isDarKTheme: false,
+        isEmailEnable: false,
+        isPushEnable: false,
+    },
+    {
+        id: 3,
+        userId: 2,
+        isDarKTheme: true,
+        isEmailEnable: false,
+        isPushEnable: true,
+    },
+]
