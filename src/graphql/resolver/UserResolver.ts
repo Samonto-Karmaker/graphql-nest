533cdd416148111bb3f8dc7ed09ef5d4ec3b04f9
Samonto Karmaker <samonto0726@gmail.com>
2025-05-12 11:29:14 +0600

    feat: implement getUserById query and refactor getUsers to use mock data

--- src/graphql/resolver/UserResolver.ts
+++ src/graphql/resolver/UserResolver.ts
@@ -1,22 +1,17 @@
-import { Query } from "@nestjs/graphql"
+import { Args, Int, Query } from "@nestjs/graphql"
 import { Resolver } from "@nestjs/graphql"
 import { User } from "../model/User"
+import { mockUsers } from "src/data/mockUsers"
 
 @Resolver()
 export class UserResolver {
     @Query(() => [User])
     getUsers() {
-        return [
-            {
-                id: 1,
-                name: "John Doe",
-                displayName: "John",
-            },
-            {
-                id: 2,
-                name: "Jane Doe",
-                displayName: "Jane",
-            },
-        ]
+        return mockUsers
+    }
+
+    @Query(() => User, { nullable: true })
+    getUserById(@Args("id", { type: () => Int }) id: number) {
+        return mockUsers.find(user => user.id === id)
     }
 }
